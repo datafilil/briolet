9d35cf6407f28f1ae6837a6527de016b5f3b84cf
datafilil <filiangelo@gmail.com>
2025-08-25 00:11:43 +0800

    feat: complete astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import keystatic from '@keystatic/astro';
-import react from '@astrojs/react'; // <-- 添加这一行
+import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare'; // <-- 添加这一行
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), keystatic(), react()], // <-- 在这里添加 react()
+	output: 'server', // <-- 重要！告诉 Astro 使用服务器端渲染
+	adapter: cloudflare(), // <-- 重要！使用 Cloudflare 适配器
+	integrations: [
+		mdx(), 
+		sitemap(), 
+		keystatic(), 
+		react()
+	],
 });
